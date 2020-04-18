8d1c2ee73056a180fe7b5597751f2bc76f9124f2
Scott beverly JR <scottbeverlyjr@Scotts-Air.fios-router.home>
2020-04-18 01:25:06 -0400

    Providers Routes

--- Routes/providerRoutes.js
+++ Routes/providerRoutes.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require('../Controllers/providerController')
+const controller = require('../Controllers/providerController');
 
-router.get('/', controller.getAllProviders)
+router.get('/', controller.getAllProviders);
 
-router.get('/:id', controller.getProviderById)
+router.get('/:name', controller.getProviderByName);
 
-router.post('/', controller.createProvider)
+router.post('/', controller.createProvider);
 
-router.put('/:id', controller.updateProvider)
+router.put('/:id', controller.updateProvider);
 
-router.delete('/:id', controller.deleteProvider)
+router.delete('/:id', controller.deleteProvider);
 
 module.exports = router;
 
