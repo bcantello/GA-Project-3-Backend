266ad98cf9a98fc5b21a6c274e2f0a368d609ab7
Scott beverly JR <scottbeverlyjr@Scotts-Air.fios-router.home>
2020-04-18 00:10:24 -0400

    userRoutes

---
+++ Routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require('../Controllers/userController')
+
+router.get('/', controller.getAllUsers)
+
+router.post('/', controller.createUser)
+
+router.put('/:id', controller.updateUser)
+
+router.delete('/:id', controller.deleteUser)
+
+module.exports = router;
