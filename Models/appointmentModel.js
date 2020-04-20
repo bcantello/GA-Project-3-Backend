8f2dd96188a771744021ea384a438ad49286759e
carlynicholson <carlypnicholson@gmail.com>
2020-04-20 11:28:42 -0500

    added DB Seed file, updated corresponding model files to accomodate for IDs

--- Models/appointmentModel.js
+++ Models/appointmentModel.js
@@ -1,10 +1,23 @@
 const mongoose = require('../DataBase/connection');
 
 const AppointmentSchema = new mongoose.Schema({
+    date: Date,
     dateTime: Date,
     service: String,
     petName: String,
-    comments: String
+    comments: String,
+    user_id: [
+        {
+            ref: "User",
+            type: mongoose.Schema.Types.ObjectId
+        }
+    ],
+    provider_id: [
+        {
+            ref: "Provider",
+            type: mongoose.Schema.Types.ObjectId
+        }
+    ],
 });
 
 Appointment = mongoose.model('Appointment', AppointmentSchema);
