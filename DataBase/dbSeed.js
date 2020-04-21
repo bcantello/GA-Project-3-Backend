8f900b037b2604c8ba96561176a2d0037355d44d
Brandon Cantello <bcantello@gmail.com>
2020-04-20 20:04:28 -0700

    formatting

--- DataBase/dbSeed.js
+++ DataBase/dbSeed.js
@@ -5,53 +5,52 @@ const Appointment = require("../Models/appointmentModel");
 const Pet = require("../Models/petModel");
 
 User.deleteMany({}).then(() => {
-  Pet.deleteMany({}).then(() => {
-    Provider.deleteMany({}).then(() => {
-      Appointment.deleteMany({}).then(() => {
-        User.create({
-          name: "Taylor Demo",
-          email: "taylor@demo.com",
-          password: "demo",
-          zip: 94120
-        }).then((taylor) => {
-          Pet.create({
-            name: "Beckett",
-            user_id: taylor._id,
-            age: 2,
-            breed: "Bernedoodle"
-          }).then((pet) => {
-            taylor.pet.push(pet._id);
-          }).then(() => {
-            Appointment.create({
-              date: "2020-05-25",
-              time: "10:30",
-              service: "Groom",
-              petID: taylor.pet[0],
-              comments:
-                  "Please don't knock, text me when you are here - he barks a lot when someone knocks!",
-              user_id: taylor._id,
-            }).then((appointment) => {
-              taylor.appointment.push(appointment._id);
-              taylor.save();
-            }).then(() => {
-              Provider.create({
-                name: "Billie",
-                reviews: 471,
-                stars: 5,
-                services: [
-                  {
-                    walk: true,
-                    groom: true,
-                    sit: false
-                  }
-                ]
-              }).then(() => {
-                process.exit();
-              });
+    Pet.deleteMany({}).then(() => {
+        Provider.deleteMany({}).then(() => {
+            Appointment.deleteMany({}).then(() => {
+                User.create({
+                    name: "Taylor Demo",
+                    email: "taylor@demo.com",
+                    password: "demo",
+                    zip: 94120
+                }).then((taylor) => {
+                    Pet.create({
+                        name: "Beckett",
+                        user_id: taylor._id,
+                        age: 2,
+                        breed: "Bernedoodle"
+                    }).then((pet) => {
+                        taylor.pet.push(pet._id);
+                    }).then(() => {
+                        Appointment.create({
+                            date: "2020-05-25",
+                            time: "10:30",
+                            service: "Groom",
+                            petID: taylor.pet[0],
+                            comments: "Please don't knock, text me when you are here - he barks a lot when someone knocks!",
+                            user_id: taylor._id,
+                        }).then((appointment) => {
+                            taylor.appointment.push(appointment._id);
+                            taylor.save();
+                        }).then(() => {
+                            Provider.create({
+                                name: "Billie",
+                                reviews: 471,
+                                stars: 5,
+                                services: [
+                                    {
+                                        walk: true,
+                                        groom: true,
+                                        sit: false
+                                    }
+                                ]
+                            }).then(() => {
+                                process.exit();
+                            });
+                        });
+                    });
+                });
             });
-          });
         });
-      });
     });
-  });
 });
